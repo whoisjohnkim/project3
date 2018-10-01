2e1f432b78fb788b12e356b600efb800b22dfa5c
llama lord <chasejrabon@gmail.com>
2018-10-01 09:23:22 -0500

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Games from './Pages/Games';
+import Games from './pages/Games';
 import Guides from './pages/Guides';
 import Food from './pages/Food';
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Login from './pages/Login';
+// // import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
 
 const App = () => (
   <Router>
     <div>
-      <Navbar />
-      <Wrapper>
+      {/* <Navbar /> */}
+      {/* <Wrapper> */}
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/signup" component={Signup} />
+        {/* <Route exact path="/signup" component={Signup} /> */}
         <Route exact path="/login" component={Login} />
         <Route exact path="/games" component={Games} />
-        <Route exact path="/players" component={Players} />
-        <Route exact path="/usernames" component={Usernames} />
+        {/* <Route exact path="/players" component={Players} /> */}
+        {/* <Route exact path="/usernames" component={Usernames} /> */}
         <Route exact path="/guides" component={Guides} />
         <Route exact path="/food" component={Food} />
-      </Wrapper>
-      <Footer />
+      {/* </Wrapper>
+      <Footer /> */}
     </div>
   </Router>
 );
---
+++ client/src/Pages/Guides.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Guides = () => (
+    <div>
+        <h1>Test</h1>
+    </div>
+);
+
+export default Guides;
