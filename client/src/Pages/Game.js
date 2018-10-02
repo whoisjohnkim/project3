3e161f98479a9bca8be9fa74a6dd62a3543e2014
nlabrada <nlabrada12@gmail.com>
2018-10-02 12:50:13 -0500

    fixed files

---
+++ client/src/Pages/Game.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import "../components/Game.css";
+import playing from "../images/playing.jpg";
+ 
+
+
+class Game extends Component {
+    render () {
+        return (
+            <div>
+                <div class="games-img">
+                <p id= "game"> just the </p>
+                <p id= "tipsy"> tipsy </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Game;
