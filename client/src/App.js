3671f8e4c085f643adb03cdda7eb0400801dba8e
llama lord <chasejrabon@gmail.com>
2018-10-02 11:16:02 -0500

    added new games to db

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,6 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import OnTap from './Pages/OnTap';
 import Home from './Pages/Home';
-<<<<<<< HEAD
-// import Game from './Pages/Game';
-// import Guides from './Pages/Guides';
-// import Food from './Pages/Food';
-// import Playlist from './Pages/Playlist';
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Wrapper from "./components/Wrapper";
-// import Jumbotron from "./components/Jumbotron";
-=======
->>>>>>> master
 
 class App extends Component {
   render() {
