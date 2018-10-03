f5bde3421b76f6b41d1ccc06222cd19c81be2bb0
Sean Angle <seanangle@me.com>
2018-10-03 10:11:53 -0500

    workign on food page

--- client/src/Pages/Food.js
+++ client/src/Pages/Food.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import "./Food/Food.css";
+import Navbar from "../components/Navbar";
 
-const divStyle = {
-    color: 'blue',
-    background: 'white'
-}
+
+
 class Food extends Component {
 
     render() {
         return (
-            <div style={{width: 800}} style={divStyle}>
+            
+            <div className= "divContainer">
+                <Navbar />
                 <h1>
                     Fooooood!!!
                 </h1>
@@ -18,12 +19,12 @@ class Food extends Component {
                     This is some of the best foods to eat while you are drinking! Or maybe have already had too much to drink!
                 </p>
 
-                <ul>
+                <ol className="list">
                     <li>Pizza!</li>
                     <li>Chips and dip!</li>
                     <li>Hamburgers!</li>
                     <li>Chili cheese dogs!</li>
-                </ul>
+                </ol>
             </div>
         )
     }
