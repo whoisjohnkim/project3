f8ea9d524e9468a0880c3e81af90079d1f828cc0
Sean Angle <seanangle@me.com>
2018-10-04 17:48:25 -0500

    changed

--- client/src/Pages/Food.js
+++ client/src/Pages/Food.js
@@ -44,7 +44,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Mac N Cheese</CardTitle>
-                            <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
+                            <CardSubtitle className="cardsubtitle">If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="100%" height="300px" src="https://i5.walmartimages.com/asr/cf3a2658-2f36-4d7d-887c-b57f7d5bdd03_1.2acac3766810ce6641b3aede6633b599.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF" alt="Card image cap" />
                             <CardBody>
@@ -59,7 +61,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Pizza Hut</CardTitle>
+                            <br></br>
                             <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="100%" height="300px" src="https://www.qsrmagazine.com/sites/qsrmagazine.com/files/styles/story_page/public/slideshow-images/slides/phut.jpg?itok=0DtjruF9" alt="Card image cap" />
                             <CardBody>
@@ -74,7 +78,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Tyson's Tacos</CardTitle>
+                            <br></br>
                             <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="100%" height="300px" src="https://static1.squarespace.com/static/57ae2aaee58c62414bd26a20/57ae31e55016e1690c423e21/57ae322ce3df28c4ce928147/1484591339729/Tyson%27s+Tacos.jpg?format=1500w" alt="Card image cap" />
                             <CardBody>
@@ -89,7 +95,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Chili Dog</CardTitle>
+                            <br></br>
                             <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="200px" height="300px" src="https://d36tnp772eyphs.cloudfront.net/blogs/1/2014/06/The-chilli-dog-600x398.jpg" alt="Card image cap" />
                             <CardBody>
@@ -104,7 +112,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Nachos</CardTitle>
+                            <br></br>
                             <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="100%" height="300px" src="https://d36tnp772eyphs.cloudfront.net/blogs/1/2014/06/Nachos_with_Chilli-600x400.jpg" alt="Card image cap" />
                             <CardBody>
@@ -119,7 +129,9 @@ class Food extends Component {
                         <Card>
                             <CardBody>
                             <CardTitle>Taco Bell</CardTitle>
+                            <br></br>
                             <CardSubtitle>If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
                             </CardBody>
                             <CardImg className="secondcardimage" width="100%" height="300px" src="https://thumbor.forbes.com/thumbor/1280x868/https%3A%2F%2Fblogs-images.forbes.com%2Fjeffhyman%2Ffiles%2F2018%2F05%2FTaco-Bell-Delivery-2015.jpg" alt="Card image cap" />
                             <CardBody>
