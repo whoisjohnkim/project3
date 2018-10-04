625c15050d6263fbccb65457a28af0b5ee9ea6d4
nlabrada <nlabrada12@gmail.com>
2018-10-04 16:18:37 -0500

    making containers for playlists

--- client/src/Pages/Playlist.js
+++ client/src/Pages/Playlist.js
@@ -8,7 +8,82 @@ class Playlist extends Component {
         return (
             <div>
                 <Navbar />
-                <div className="playlist-img"></div>
+                <div className="playlist-img">
+                    <div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col">                            
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col">                            
+                                <div className="card">
+                                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+
+                    </div>
+               </div>
             </div>
         )
     }
