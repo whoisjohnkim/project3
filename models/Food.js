45dc80322fad4b57ef03cc0366a339670e9a49b4
Sean Angle <seanangle@me.com>
2018-10-04 11:42:52 -0500

    Created a foodcard component

--- models/Food.js
+++ models/Food.js
@@ -4,5 +4,32 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var FoodSchema = new Schema({
-    
+    //Title of the food
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    //Image of the food
+    picture: {
+        type: String,
+        required: false,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: false,
+        unique: true
+    }
 });
+
+//Create model from above schema
+var Food = mongoose.model("Food",FoodSchema);
+
+//Export the Game model
+module.exports = Food;
