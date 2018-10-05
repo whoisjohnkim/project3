f283aa5ff38813cb0af899a3100b7ccd7d6f2b69
Nanette Labrada <39708165+nlabrada@users.noreply.github.com>
2018-10-05 12:10:23 -0500

    Revert "John kim"

--- client/src/Pages/Game.js
+++ client/src/Pages/Game.js
@@ -16,18 +16,12 @@ class Game extends Component {
         turns: [],
         order: [],
         turnNumber: 0,
-        currentTurn: "",
-        phase: "title"
+        currentTurn: ""
     };
 
     componentDidMount() {
-        // this.loadGame();
-        this.handleChange = this.handleChange.bind(this);
+        this.loadGame();
     };
-
-    clearPlayers = () => {
-        this.setState({players: []});
-    }
 
     loadGame = () => {
         // API.getInteractive()
@@ -39,14 +33,9 @@ class Game extends Component {
             title: game.title,
             description: game.description,
             turns: game.turns,
-            players: ["Player1", "Player2", "Player3", "Player4"],
-            phase: "started"
+            players: ["Player1", "Player2", "Player3", "Player4"]
         })
         this.setOrder();
-    };
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
     };
 
     setOrder = () => {
@@ -54,7 +43,7 @@ class Game extends Component {
         for(var i = 0; i < this.state.turns.length; i++){
             order.push(i);
         }
-        this.setState({order: order, turnNumber: 0, currentTurn: "Everyone Take A Drink!"});
+        this.setState({order: order, turnNumber: 0, currentTurn: "Start the Game to Get Just the Tipsy!"});
         this.shuffle();
     };
 
@@ -89,34 +78,24 @@ class Game extends Component {
             this.setState({currentTurn: this.state.players[random1] + this.state.turns[this.state.turnNumber] + this.state.players[random2], turnNumber: this.state.turnNumber + 1});
         }
 
-    };
+    }
+
     render () {
-        let display;
-        if(this.state.phase === "started"){
-            display = (<div className="games-img" onClick={this.nextTurn}>
-                        <p id="gameText">{this.state.currentTurn}</p>
-                    </div>)
-        }
-        else if(this.state.phase === "title"){
-            display = (<div className="games-img" onClick={this.loadGame}>
-                <p id= "game"> just the </p>
-                <p id= "tipsy"> tipsy </p>
-            </div>)
-        }
-        else if(this.state.phase === "players"){
-            display = (<div className="games-img">
-                <input type="text" value={this.state.playerName} onChange={this.handleChange}/>
-                <button type="button" onClick={this.addPlayer}>Add Player</button>
-                <button type="button" onClick={this.clearPlayers}>Clear Players</button>
-            </div>)
-        }
         return (
             <div>
                 <Navbar />
-                {display}
+                <div className="games-img">
+                    <p id= "game"> just the </p>
+                    <p id= "tipsy"> tipsy </p>
+                </div>
+                <div>
+                    <h3>{this.state.title}</h3>
+                    <h4>{this.state.currentTurn}</h4>
+                    <p>{this.state.description}</p>
+                </div>
+                <button type="button" onClick={this.nextTurn}>Next Turn!</button>
             </div>
         )
-
     }
 }
 
