642728d47c245ccb8a96a46f8a29355d9163d783
llama lord <chasejrabon@gmail.com>
2018-10-08 12:35:21 -0500

    updating signup

--- routes/api/logins.js
+++ routes/api/logins.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const loginController = require("../../controllers/loginController");
 
-router.route("/signup")
-    .get(registersController.create)
-    // .post(registersController.create);
+router.route("/login")
+    .get(registersController.findAll)
+    
    
 
 module.exports = router;
