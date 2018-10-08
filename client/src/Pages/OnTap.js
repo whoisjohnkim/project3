0ce7fd2aa7026a6bde325707735fb128706bb2a7
KirdMNKY <mr.mike.soto@gmail.com>
2018-10-08 15:05:46 -0500

    navbar

--- client/src/Pages/OnTap.js
+++ client/src/Pages/OnTap.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
 import "../components/OnTap/OnTap.css";
-<<<<<<< HEAD
-import logo from "../images/OnTapLogoUpdated.png";
-=======
 const landinglogo = require('../images/OnTapLogoUpdated.png');
 
->>>>>>> master
 
 class OnTap extends Component {
     render() {
@@ -16,13 +12,6 @@ class OnTap extends Component {
                         {/* <img id= "logo" src={require('../images/OnTapLogo.svg')} /> */}
                         <img id= "circle" src={require('../images/circle.png')} />
                         <div className="text-center same-stuff">
-<<<<<<< HEAD
-                         
-                         <img id="MainLogo" src={ logo } alt="logo" OnClick="/signup" />                           
-                            <h4 id= "toplay"> click to play! </h4>
-                         
-                                                    
-=======
                             {/* <h1 id= "ontapword"> ON TAP </h1>                             */}
                             <img id="landinglogo" src={ landinglogo } />
                             <br/>
@@ -30,7 +19,6 @@ class OnTap extends Component {
                                 <img id= "smolbeer" src={require('../images/smolbeer.png')} />
                             </a>
                             <h4 id= "toplay"> click to play! </h4>                        
->>>>>>> master
                         </div>
                     </div>
                 </div>
