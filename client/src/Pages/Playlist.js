0e5d3cae390e5ccc08f0ad45c627009e08395b37
Sean Angle <seanangle@me.com>
2018-10-08 10:19:04 -0500

    committing so I can merge

--- client/src/Pages/Playlist.js
+++ client/src/Pages/Playlist.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import Navbar from "../components/Navbar";
 import "../components/Playlist.css";
+import { Card, CardImg, CardText, CardBody, CardLink,
+CardTitle, CardSubtitle, Button } from 'reactstrap';
+import "./Playlist/Playlist.css";
 
 
 class Playlist extends Component {
@@ -8,67 +11,47 @@ class Playlist extends Component {
         return (
             <div>
                 <Navbar />
-                <div className="playlist-img">
-                    <div>
-                        <div className="row">
-                            <div className="col">
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                    <iframe id="playlist" src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXcBWIGoYBM5M" width="1680" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> 
-                                        {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div className="col">
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
+                <div className='playlistcard'>
+                    <Card>
+                        <CardBody>
+                            <CardTitle className="cardTitle"><iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXdgnLr18vPvu" width="410" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></CardTitle>
+                            <br></br>
+                            <CardSubtitle className="cardsubtitle">If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
+                        </CardBody>
+                    </Card>
+                </div>
+            
 
-                            <div className="col">                            
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                        
-                        </div>
+            
+                <div className='playlistcard'>
+                    <Card>
+                        <CardBody>
+                            <CardTitle className="cardTitle"><iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX4WYpdgoIcn6" width="410" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></CardTitle>
+                            <br></br>
+                            <CardSubtitle className="cardsubtitle">If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
+                        </CardBody>
+                    </Card>
+                </div>
+            
 
-                        <div className="row">
-                            <div className="col">
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col">
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col">                            
-                                <div className="card card-playlist">
-                                    <div className="card-body">
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                        
-                        </div>
-
-                    </div>
-               </div>
-                <div className="playlist-img"></div>
+                                        
+                <div className='playlistcard'>
+                    <Card>
+                        <CardBody>
+                            <CardTitle className="cardTitle"><iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX0XUsuxWHRQd" width="410" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></CardTitle>
+                            <br></br>
+                            <CardSubtitle className="cardsubtitle">If you haven't had it, you have to try it!</CardSubtitle>
+                            <br></br>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
+                           
+                        
+                        
         )
     }
 }
