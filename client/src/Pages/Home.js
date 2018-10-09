f9b734d4e527d3699e3a3e5fa86d45f6cb79a0c8
nlabrada <nlabrada12@gmail.com>
2018-10-09 10:01:46 -0500

    cool

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
                 <span class="part part--center">
                     <span class="letter letter--i">I</span>
                     {/* <span class="letter letter--o">A</span> */}
-                    <span class="letter letter--U">U</span>
+                    {/* <span class="letter letter--U">U</span> */}
                 </span>
                 <span className="part part--right">
                     <span className="letter">N</span>
